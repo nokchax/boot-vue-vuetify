70a17edd22993560e469c175f437299be084d7ba
rogue <retrokarr@gmail.com>
2018-06-17 19:39:29 +0900

    first commit

---
+++ src/main/java/com/vuetify/gettingstart/GettingstartApplication.java
@@ -0,0 +1,12 @@
+package com.vuetify.gettingstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GettingstartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GettingstartApplication.class, args);
+    }
+}
